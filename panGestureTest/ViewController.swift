e7f61a450a51d1639bfb5845ee00a72ae23028cf
tax_k <taxfree9090@gmail.com>
2018-10-16 11:57:02 +0900

    Edit Gesture Conflict problem at the same time 🤩🤪🙌🏻👊🏻

--- panGestureTest/ViewController.swift
+++ panGestureTest/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum panAxis : Int {
+    case upDown
+    case leftRight
+    case defaults
+}
+
+class ViewController: UIViewController, UIGestureRecognizerDelegate{
+    
+    var axis:Int = 0
+    var startingPoint:CGPoint?
+    var startingCenter:CGPoint?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(didSwipeOnView))
+        panGesture.delegate = self
+        self.view.addGestureRecognizer(panGesture)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func didSwipeOnView(gestureRecognizer: UIPanGestureRecognizer) {
+       let testPoint: CGPoint = gestureRecognizer.location(in: view)
+        print(testPoint)
+        switch gestureRecognizer.state {
+        case .began:
+            axis = panAxis.defaults.rawValue
+            startingPoint = testPoint
+            startingCenter = gestureRecognizer.view?.center
+        case .changed:
+            switch axis {
+            case panAxis.defaults.rawValue:
+                if fabsf(Float(testPoint.x - startingPoint!.x)) > fabsf(Float(testPoint.y - startingPoint!.y)) && fabs(Float(testPoint.x - (startingPoint?.x)!)) > 20 {
+                    axis = panAxis.leftRight.rawValue
+                }else if fabsf(Float(testPoint.x - startingPoint!.x)) < fabsf(Float(testPoint.y - startingPoint!.y)) && fabs(Float(testPoint.y - (startingPoint?.y)!)) > 20 {
+                    axis = panAxis.upDown.rawValue
+                }
+             case panAxis.leftRight.rawValue:
+                print("hr")
+            case panAxis.upDown.rawValue:
+                print("vr")
+            default:
+                break
+            }
+        case .ended:
+            axis = panAxis.defaults.rawValue
+        default:
+            break
+        }
     }
-
-
 }
-
